b6d3b1b55e1d5edaf20fd59a0da593b4ed4f2086
Nodarra <sisicdino4@gmail.com>
2021-03-25 18:57:55 +0100

    Defined app structure

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../Styles/Sidebar.css'
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      Sidebar
+      <div className="sidebar-header">
+        <div className="header-right">
+        
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAnenMM3bX3XDxwgkhhhQZxdziWvMoSa1c",
+  authDomain: "whatsapp-mern-1d3a8.firebaseapp.com",
+  projectId: "whatsapp-mern-1d3a8",
+  storageBucket: "whatsapp-mern-1d3a8.appspot.com",
+  messagingSenderId: "796105205109",
+  appId: "1:796105205109:web:387a85564b18feacb45f3c"
+};
