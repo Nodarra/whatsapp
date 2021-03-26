fa90c7ffab04bc7bb13ecff3c1b04ae33e87be9d
Nodarra <sisicdino4@gmail.com>
2021-03-26 19:32:35 +0100

    Added Chat header section

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,10 +1,32 @@
 import React from 'react'
 import '../Styles/Chat.css'
+import { Avatar } from '@material-ui/core'
+import { IconButton } from '@material-ui/core'
+import { AttachFile, MoreVert, SearchOutlined} from '@material-ui/icons';
 
 function Chat() {
   return (
     <div className="chat">
-      Chat
+      <div className="chat-header">
+        <Avatar />
+
+        <div className="chat-header-info">
+          <h3>Room name</h3>
+          <p>Last seen at...</p>
+        </div>
+
+        <div className="chat-header-right">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
     </div>
   )
 }
