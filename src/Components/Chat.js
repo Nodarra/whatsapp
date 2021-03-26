3262439c011cdf9f164fa7eeed27bc1bf3b48996
Nodarra <sisicdino4@gmail.com>
2021-03-26 20:22:24 +0100

    Added chat window nad chat input

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -2,7 +2,7 @@ import React from 'react'
 import '../Styles/Chat.css'
 import { Avatar } from '@material-ui/core'
 import { IconButton } from '@material-ui/core'
-import { AttachFile, MoreVert, SearchOutlined} from '@material-ui/icons';
+import { AttachFile, MoreVert, SearchOutlined, InsertEmoticon, Send, Mic } from '@material-ui/icons';
 
 function Chat() {
   return (
@@ -26,6 +26,38 @@ function Chat() {
             <MoreVert />
           </IconButton>
         </div>
+      </div>
+
+      <div className="chat-body">
+        <p className="chat-message">
+          <span className="chat-name">Nodarra</span>
+          This is a message
+          <span className="timestamp">{new Date().toUTCString()}</span>
+        </p>
+
+        <p className="chat-message chat-sent">
+          <span className="chat-name">Nodarra</span>
+          This is a message
+          <span className="timestamp">{new Date().toUTCString()}</span>
+        </p>
+      </div>
+
+      <div className="chat-input">
+        <IconButton>
+          <InsertEmoticon />
+        </IconButton>
+        <form>
+          <input 
+            placeholder="Type a message"
+            type="text"
+          />
+          <IconButton type="submit">
+            <Send />
+          </IconButton>
+          <IconButton >
+            <Mic />
+          </IconButton>
+        </form>
       </div>
     </div>
   )
